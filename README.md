4ff84862b91c1d5c000ef5b8351afd97ccb606fb
kentgeek <86157540@qq.com>
2020-11-15 22:52:21 +0800

    移动文件夹后第一次提交

---
+++ README.md
@@ -0,0 +1,3 @@
+# alien-invasion
+A simple game.
+简单的小游戏。
---
+++ settings.py
@@ -0,0 +1,28 @@
+class Settings():
+    
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+      
+        self.fleet_drop_speed = 10
+        self.speedup_scale = 1.1
+        self.initialize_dynamic_settings()
+        
+        self.ship_limit = 3
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
